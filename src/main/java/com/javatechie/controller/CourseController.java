57c588e1d3046c1f98c6c6afa752256d018dc460
suryaarigela <suri243@gmail.com>
2025-12-26 21:40:17 -0500

    initial commit

--- src/main/java/com/javatechie/controller/CourseController.java
+++ src/main/java/com/javatechie/controller/CourseController.java
@@ -57,16 +57,14 @@ public class CourseController {
         }
     }
 
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "UP";
-    }
+	/*
+	 * @GetMapping("/health") public String healthCheck() { return "UP"; }
+	 */
 
 
-    @GetMapping("/welcome")
-    public String greetings() {
-        return "Hello Techie , AWS CICD Example working fine !";
-    }
-
+	/*
+	 * @GetMapping("/welcome") public String greetings() { return
+	 * "Hello Techie , AWS CICD Example working fine !"; }
+	 */
 
 }
