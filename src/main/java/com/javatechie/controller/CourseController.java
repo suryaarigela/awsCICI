3807f3cb12daff3f218558664f9a35da7e585fec
suryaarigela <suri243@gmail.com>
2025-12-27 13:14:22 -0500

    test commit

--- src/main/java/com/javatechie/controller/CourseController.java
+++ src/main/java/com/javatechie/controller/CourseController.java
@@ -64,7 +64,7 @@ public class CourseController {
 
 	
 	  @GetMapping("/welcome") public String greetings() { return
-	  "Hello Techie , AWS CICD Example working fine !"; }
+	  "Hello Surya , AWS CICD Example working fine !"; }
 	 
 
 }
